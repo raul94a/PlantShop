b3b6b517a3e8d6193ad7a3f67487d5d69cc5f433
raul.albin <raul200794@gmail.com>
2024-11-13 15:05:59 +0100

    Add gradient to banner

--- app/src/main/java/com/raul/plantshop/presentation/plants/DiscountBanner.kt
+++ app/src/main/java/com/raul/plantshop/presentation/plants/DiscountBanner.kt
@@ -1,6 +1,7 @@
 package com.raul.plantshop.presentation.plants
 
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,13 +12,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -27,7 +38,9 @@ import androidx.compose.ui.unit.sp
 import com.raul.plantshop.R
 import com.raul.plantshop.ui.theme.Typography
 import com.raul.plantshop.ui.theme.bannerGreen
+import com.raul.plantshop.ui.theme.buttonColor
 import com.raul.plantshop.ui.theme.headerGrey
+import com.raul.plantshop.ui.theme.mainBlack
 import com.raul.plantshop.ui.theme.subtitle
 
 
@@ -36,14 +49,21 @@ data class Discount(
 )
 
 @Composable
-fun DiscountBanner(modifier: Modifier,discount: Discount) {
+fun DiscountBanner(modifier: Modifier, discount: Discount) {
 
     val bannerHeight = remember { 175.dp }
     Box(
         modifier = modifier
             .fillMaxWidth()
             .height(bannerHeight)
-            .padding(horizontal = 15.dp).padding(bottom = 10.dp)
+            .padding(horizontal = 15.dp)
+            .padding(bottom = 10.dp)
+            .shadow(
+                elevation = 16.dp,
+                shape = RoundedCornerShape(16.dp),
+                spotColor = Color.Black.copy(alpha = 0.52f),
+                ambientColor = Color.Black.copy(alpha = 0.1f)
+            )
 
 
     ) {
@@ -53,9 +73,16 @@ fun DiscountBanner(modifier: Modifier,discount: Discount) {
             modifier = Modifier
                 .padding(top = 25.dp)
                 .fillMaxSize()
+                .background(
+                    brush = Brush.horizontalGradient(listOf(bannerGreen, Color.White)),
+                    shape = RoundedCornerShape(18.dp)
 
-                .background(color = bannerGreen, shape = RoundedCornerShape(18.dp))
+                )
+
+
         ) {
+
+            }
             Column(
                 modifier = Modifier
                     .padding(20.dp)
@@ -79,7 +106,7 @@ fun DiscountBanner(modifier: Modifier,discount: Discount) {
             }
 
 
-        }
+
         Image(
             painter = discount.imagePainter,
             contentDescription = "Discount Image Banner",
@@ -101,3 +128,4 @@ fun DiscountBanner(modifier: Modifier,discount: Discount) {
 
 
 }
+
